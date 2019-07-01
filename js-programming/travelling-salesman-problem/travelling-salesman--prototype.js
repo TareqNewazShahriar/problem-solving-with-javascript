15504215003c12e7f2d0e8fd9f68cee12edd75bc
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2019-07-01 16:44:07 +0600

    in progress

--- js-programming/travelling-salesman-problem/travelling-salesman--prototype.js
+++ js-programming/travelling-salesman-problem/travelling-salesman--prototype.js
@@ -5,13 +5,13 @@ var distances_2d = [ [], [], [] ];
 function recursion(remain_list, total_distance_so_far, last_point)
 {
     let min = Infinity;
-    let total_distance = Infinity;
+    let distance = Infinity;
     for (let i = 1; i < remain_list.length; i++) {
         let cloneList = remain_list.slice();
         cloneList.splice(i, 1);
-        total_distance = recursion(cloneList, total_distance_so_far + distances_2d[last_point, i]);
-        if(total_distance < min)
-            min = total_distance;
+        distance = recursion(cloneList, total_distance_so_far + distances_2d[last_point, i]);
+        if(distance < min)
+            min = distance;
     }
 
     return min;
